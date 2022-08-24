6a0d45c097c697c66289efc1b64d5e4201c663b8
Mariano Filho <mariano.csfilho@gmail.com>
2022-08-24 16:17:11 -0300

    desafio 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,8 +3,10 @@ let email = "mariano.csfilho@gmail.com"
 let profissao = "AUX TI"
 let idade = 41
 let localSonhos = "Japão"
-console.log (nome)
-console.log (email)
-console.log (profissao)
-console.log (idade)
-console.log (localSonhos)
+console.log (`Nome: ${nome} 
+Email: ${email} 
+Profissão: ${profissao} 
+Idade: ${idade} 
+Viagem dos sonhos: ${localSonhos}`)
+
+
