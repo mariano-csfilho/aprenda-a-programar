5826896ef5ee4661bb4e347d7d6712addc0bb476
Mariano Filho <mariano.csfilho@gmail.com>
2022-08-24 16:26:23 -0300

    desafio 1 template strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,10 +3,6 @@ let email = "mariano.csfilho@gmail.com"
 let profissao = "AUX TI"
 let idade = 41
 let localSonhos = "Japão"
-console.log (`Nome: ${nome} 
-Email: ${email} 
-Profissão: ${profissao} 
-Idade: ${idade} 
-Viagem dos sonhos: ${localSonhos}`)
+console.log (`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para o ${localSonhos}. Confirma pra gente alguns dados? A sua idade é ${idade} anos, você é ${profissao}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`); 
 
 
