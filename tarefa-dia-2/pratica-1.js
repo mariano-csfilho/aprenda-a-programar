4dd8601e2ae24eec38b7cbf18b2266d2951ce0fd
Mariano Filho <mariano.csfilho@gmail.com>
2022-08-24 15:17:54 -0300

    atualizando variaveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,4 +3,8 @@ let email = "mariano.csfilho@gmail.com"
 let profissao = "AUX TI"
 let idade = 41
 let localSonhos = "Japão"
-
+console.log (nome)
+console.log (email)
+console.log (profissao)
+console.log (idade)
+console.log (localSonhos)
